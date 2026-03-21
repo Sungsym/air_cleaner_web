4c849e87b253e0de3361e8b74b46ace19dcc95f6
Sungsym <sunyiyang523@outlook.com>
2026-03-21 23:21:20 +0800

    数据库

--- app/config.py
+++ app/config.py
@@ -3,14 +3,11 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 class DatabaseSetting(BaseSettings):
     POSTGRES_URL: str
-    POSTGRES_ASYNC_URL: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
 
     # 让它直接读取同名的环境变量（Vercel 中已设置）
     model_config = SettingsConfigDict(
-        env_prefix="",          # vercel
-        # env_file=".env",       # 本地
+        # env_prefix="",          # vercel
+        env_file=".env",       # 本地
         case_sensitive=True,   # 大小写敏感
         env_ignore_empty = True,
         extra = "ignore",
