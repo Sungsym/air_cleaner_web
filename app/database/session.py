3f4e09c98b1781bcac2170e6e64d598b7a805c22
Sungsym <sunyiyang523@outlook.com>
2026-03-21 21:10:26 +0800

    本地postgresql测试

--- app/database/session.py
+++ app/database/session.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
+from app.config import settings
 
-
-db = "sqlite:///database.db"
-engine = create_engine(db, echo=True)
+# db_url = "sqlite:///database.db"
+db_url = settings.POSTGRES_URL
+engine = create_engine(db_url, echo=True)
 
 def create_db_and_tables():
     from app.database.models import Air
@@ -12,3 +13,4 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
